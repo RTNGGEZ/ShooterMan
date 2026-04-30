79463fbb91adfcfef8fb7cc104a8d1c5560fe6e3
K0ppe <133238048+K0ppe@users.noreply.github.com>
2026-04-30 15:17:27 +0900

    旧Pコントローラーを削除

--- Assets/Resource/PlayerScript/FollowCam.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class FollowCam : MonoBehaviour {
-    [SerializeField] private Transform target;
-
-    private void Update() {
-        transform.position = target.position;
-    }
-}
